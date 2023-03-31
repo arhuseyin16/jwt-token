8eab0f9979537114a40b6f7ee9d7f97ca4ed5fa7
Hüseyin Ar <arhuseyin16@gmail.com>
2023-03-31 19:18:51 +0300

    jwt-token nedir? Nasıl jsondan key olusur?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-jwt-token';
+  token: string | null = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJUaXRsZSI6IkZyb250ZW5kIERldmVsb3BlciIsIkRlcGFydG1lbnQiOiJ" +
+    "CaWxnaSBTaXN0ZW1sZXJpIiwiRGlzcGxheU5hbWUiOiJIdXNleWluIEFSIiwiQWNjb3VudE5hbWUiOiJodXNleWluLmFyIiwiVXNlckVtYWlsIjoiaHVzZXlpbi5hc" +
+    "kBiZXltZW4uY29tIiwiVXNlckNvZGUiOiJodXNleWluLmFyIiwicm9sZSI6WyJQb2x5Z29uQWRtaW4iLCJUcmFja2luZ09yZGVyVXNlciIsIlN0b3JlVmlzaXRBZG1p" +
+    "biIsIkFkbWluIiwiQ2FyZ29BZG1pbiJdLCJuYmYi" +
+    "OjE2NzgyNzUyMjQsImV4cCI6MTY3OTEzOTIyNCwiaWF0IjoxNjc4Mjc1MjI0fQ.yb00_Ce_Q8wJytoo0AM1aDBD-tQQNBPL0XVXiegOtKQ";
+  table_Key: any[] = [];
+  table_Value: any[] = [];
+
+  converter() {
+    var base64Url = this.token?.split('.')[1];
+    var base64 = base64Url?.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(window.atob(base64 ? base64 : '').split('').map(function(c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+  }
+
+  jwtPayload() {
+    this.table_Value = [];
+    this.table_Key = [];
+    this.table_Key = Object.keys(this.converter());
+    this.table_Value = Object.values(this.converter());
+  }
 }
